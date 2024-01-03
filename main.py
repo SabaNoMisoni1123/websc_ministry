8e87eaf3e3a37e72311f9f55f1b288a7639faf67
sabamiso <s.kondo11235813@gmail.com>
2024-01-04 06:08:55 +0900

    cros対策

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:5173",
+    "http://localhost:4173",
+    "*",
 ]
 app.add_middleware(
     CORSMiddleware,
